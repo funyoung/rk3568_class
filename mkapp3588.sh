15d40fd5a54ba8507e413dee3c56c5e06a28b7ab
yangfeng <e13310@gmail.com>
2024-12-11 11:41:34 +0800

    add 10_private_data to present private data using with file struct

--- mkapp3588.sh
+++ mkapp3588.sh
@@ -1,3 +1,12 @@
 #/bin/sh
 
-/home/topeet/work/rk356x_linux/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc -o app $@ -static
+src="app.c"
+if [ $# -gt 0 ]; then
+#    echo "提供了多个参数。"
+    src=$@
+else
+#    echo "参数个数不大于0。"
+    src="app.c"
+fi
+
+/home/topeet/work/rk356x_linux/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc -o app $src -static
