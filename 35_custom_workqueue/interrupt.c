5f6b337f16513121b7e59e6b6772fb64e0ec43f7
yangfeng <e13310@gmail.com>
2024-12-14 18:46:36 +0800

    clone 35_custom_workqueue from 30_interrupt to test bottom half of interrupt with custom work queue.

---
+++ 35_custom_workqueue/interrupt.c
@@ -0,0 +1,72 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
+#include <linux/delay.h>
+#include <linux/workqueue.h>
+
+/* GPIO3_A5: 3*32 + 0 * 8 + 5 = 101 */
+#define GPIO_PIN 101
+
+/* 自定义工作队列和工作项和中断下文工作项处理函数 */
+static struct workqueue_struct *test_workqueue;
+static struct work_struct test_workqueue_work;
+static void test_work(struct work_struct *work)
+{
+    msleep(1500);
+    printk(KERN_INFO "this is test_work\n");
+}
+
+static irqreturn_t gpio_irq_handler(int irq, void *dev_id)
+{
+    printk(KERN_INFO "Interrupt occurs: %d\n", irq);
+    printk(KERN_INFO "this is interrupt handler.\n");
+
+    /* 提交工作项到自定义工作队列 */
+    queue_work(test_workqueue, &test_workqueue_work);
+    return IRQ_RETVAL(IRQ_HANDLED);
+}
+
+/* 引脚映射成中断号，请求中断 */
+static int __init interrupt_init(void)
+{
+    int irq_num;
+
+    printk(KERN_INFO "Initializing GPIO Interrupt Driver\n");
+    irq_num = gpio_to_irq(GPIO_PIN);
+    printk(KERN_INFO "GPIO %d map to irq %d\n", GPIO_PIN, irq_num);
+
+    if (request_irq(irq_num, gpio_irq_handler, IRQF_TRIGGER_RISING, "irq_test",  NULL) != 0) {
+        printk(KERN_INFO "Failed to request IRQ %d\n", irq_num);
+        gpio_free(GPIO_PIN);
+        return -ENODEV;
+    }
+
+    /* 创建工作队列并初始化工作项 */
+    test_workqueue = create_workqueue("test_workqueue");
+    INIT_WORK(&test_workqueue_work, test_work);
+
+
+    return 0;
+}
+
+static void __exit interrupt_exit(void)
+{
+    int irq_num;
+    irq_num = gpio_to_irq(GPIO_PIN);
+    free_irq(irq_num, NULL);
+
+    /* 取消工作项，刷新队列，销毁工作队列*/
+    cancel_work_sync(&test_workqueue_work);
+    flush_workqueue(test_workqueue);
+    destroy_workqueue(test_workqueue);
+
+    printk(KERN_INFO "GPIO Interrupt Driver exit successully.\n");
+}
+
+module_init(interrupt_init);
+module_exit(interrupt_exit);
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("YF");
+MODULE_VERSION("V1.0");
